8c55f3f3b611c14111913e29d57cc082ee4acdea
Kirill <User@WIN-9507BK6FFVC>
2026-06-23 23:04:19 +0600

    Added personal wallet query

---
+++ Fast_Change_Backend/Application/Features/Wallets/GetUserWallets/GetUserWalletsQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Features.Users.RegisterUser;
+using MediatR;
+using Resources;
+
+namespace Application.Features.Wallets.GetUserWallets;
+
+public sealed class GetUserWalletsQueryHandler
+    : IRequestHandler<GetUserWalletsQuery, List<GetUserWalletsResponse>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ICurrentUserService _currentUser;
+
+    public GetUserWalletsQueryHandler(
+        IUserRepository userRepository,
+        ICurrentUserService currentUser)
+    {
+        _userRepository = userRepository;
+        _currentUser = currentUser;
+    }
+
+    public async Task<List<GetUserWalletsResponse>> Handle(
+        GetUserWalletsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(
+            _currentUser.UserId,
+            cancellationToken);
+
+        if (user is null)
+            throw new BusinessException(Localization.UserNotFound);
+
+        return user.Wallets
+            .Select(x => new GetUserWalletsResponse(
+                x.Id,
+                x.Currency,
+                x.Balance))
+            .ToList();
+    }
+}
---
+++ Fast_Change_Backend/Application/Features/Wallets/GetUserWallets/GetUserWalletsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Features.Wallets.GetUserWallets;
+
+public sealed record GetUserWalletsResponse(
+    Guid WalletId,
+    string Currency,
+    decimal Balance);
