4843197662d93081e46f6575b3f17f00e7be4b5c
Kirill <User@WIN-9507BK6FFVC>
2026-07-04 22:31:42 +0600

    Added SignalR hubs

---
+++ Fast_Change_Backend/Infrastructure/SignalR/Services/WalletNotificationService.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Infrastructure.SignalR.Services;
+
+public sealed class WalletNotificationService
+    : IWalletNotificationService
+{
+    private readonly IHubContext<WalletHub> _hub;
+
+    public WalletNotificationService(
+        IHubContext<WalletHub> hub)
+    {
+        _hub = hub;
+    }
+
+    public async Task WalletUpdatedAsync(
+        Guid walletId,
+        CancellationToken cancellationToken)
+    {
+        await _hub.Clients
+            .User(walletId.ToString())
+            .SendAsync(
+                "WalletUpdated",
+                new
+                {
+                    WalletId = walletId
+                },
+                cancellationToken);
+    }
+}
