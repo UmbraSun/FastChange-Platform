1808e5d481822660b83f9d911da6d48693e17844
Kirill <User@WIN-9507BK6FFVC>
2026-07-06 21:58:04 +0600

    Added multi-channel notification

---
+++ Fast_Change_Backend/Infrastructure/Notifications/SignalRNotificationChannel.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using Contracts.Events;
+using Contracts.Notifications;
+
+namespace Infrastructure.Notifications;
+
+public sealed class SignalRNotificationChannel
+    : IExchangeNotificationChannel
+{
+    private readonly IWalletNotificationService _notificationService;
+
+    public SignalRNotificationChannel(
+        IWalletNotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    public async Task NotifyAsync(
+        ExchangeCompletedEvent @event,
+        CancellationToken cancellationToken)
+    {
+        await _notificationService.WalletUpdatedAsync(
+            @event.FromWalletId,
+            cancellationToken);
+
+        await _notificationService.WalletUpdatedAsync(
+            @event.ToWalletId,
+            cancellationToken);
+    }
+}
